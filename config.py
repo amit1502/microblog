3c15122466cf587ff8dd00166e2267b6ea107164
amit1502 <meap1718@gmail.com>
2025-03-31 18:41:56 +0530

    Adding User, Post Models

--- config.py
+++ config.py
@@ -3,4 +3,7 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
 
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
+    'mysql+pymysql://root:admin@10.239.139.170:3306/microblog'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
