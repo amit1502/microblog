c87e58867000312868f3abbefbc100420993ea8f
amit1502 <meap1718@gmail.com>
2025-04-01 09:39:59 +0530

    registering and login user

--- app/models.py
+++ app/models.py
@@ -4,14 +4,29 @@ user (id, username, email, passwordhash)
 from app import db
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+from flask_login import UserMixin
+from app import login
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     username = Column(String(64), index=True, unique=True)
     email = Column(String(120), index=True, unique=True)
-    password_hash = Column(String(128))
+    password_hash = Column(String(500))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User:{}>'.format(self.username)
